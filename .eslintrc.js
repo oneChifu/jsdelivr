3ca185203f71f89d192da095b96f5a3473b7dbc3
onechifu <onechifu@gmail.com>
2022-12-29 03:57:15 +0200

    Base template

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,36 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    parser: "@babel/eslint-parser",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
-      env: {
-        jest: true,
-      },
+    root: true,
+    env: {
+        node: true,
     },
-  ],
+    extends: [
+        'plugin:vue/essential',
+        'eslint:recommended',
+        '@vue/eslint-config-prettier',
+        'prettier',
+        'plugin:prettier/recommended',
+        'plugin:node-dependencies/recommended',
+        'plugin:jsonc/recommended-with-jsonc',
+    ],
+    parserOptions: {
+        parser: '@babel/eslint-parser',
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-unused-vars': process.NODE_ENV === 'production' ? 'error' : 'warn',
+        'no-restricted-imports': 'off',
+        'prettier/prettier': ['error', {}],
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)',
+            ],
+            env: {
+                jest: true,
+            },
+        },
+    ],
 };
