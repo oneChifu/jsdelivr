e3efbb0b1d833a77d95a9bf9ce26620e5130e1aa
Pavlo Lavrus <onechifu@gmail.com>
2023-01-04 16:08:34 +0200

    Fix for deploy

--- deploy.sh
+++ deploy.sh
@@ -15,12 +15,11 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:onechifu/jsdelivr.git main:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# git push -f git@github.com:onechifu/onechifu.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:onechifu/jsdelivr.git main:gh-pages
 
 cd -
